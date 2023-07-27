4135ce1b9ca19f781fccc947b1eefdd960c4122e
Anselm Kiefner <github@anselm.kiefner.de>
2023-07-27 23:14:22 +0200

    initial

---
+++ server/server.py
@@ -0,0 +1,25 @@
+import time
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print(f"Received request: {message}")
+
+    #  Do some 'work'
+    time.sleep(1)
+
+    #  Send reply back to client
+    socket.send(b"World")
+
+    # catch ctrl+c and quit
+    try:
+        pass
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed, exiting.")
+        break
